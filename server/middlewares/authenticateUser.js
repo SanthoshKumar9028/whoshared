f25366a7d14ab66ae98af24ab142566f5d53b22a
SanthoshKumar9028 <71515865+SanthoshKumar9028@users.noreply.github.com>
2021-02-09 21:05:53 +0530

    added user authentication for web socket

--- server/middlewares/authenticateUser.js
+++ server/middlewares/authenticateUser.js
@@ -8,12 +8,23 @@ export function blockUnauthorizedUser(req, res, next) {
   res.json({ username: "unauthorized user" });
 }
 
+export function authenticateUserForWS(request, next = () => {}) {
+  let cookie = request.headers.cookie || "";
+  let cookies = cookie.split(",").map((c) => c.trim().split("="));
+  let jwttoken = cookies.find(([key]) => key === "jwt");
+  if (jwttoken) {
+    jwt.verify(jwttoken[1], process.env.JWT_SECRET, (err, decodedData) => {
+      next(err, decodedData);
+    });
+  } else next(null);
+}
+
 export default function (req, res, next) {
-  const token = req.cookies.jwt;
+  const cookies = req.cookies;
   req._user_ = null;
 
-  if (token) {
-    jwt.verify(token, process.env.JWT_SECRET, (err, decodedData) => {
+  if (cookies && cookies.jwt) {
+    jwt.verify(cookies.jwt, process.env.JWT_SECRET, (err, decodedData) => {
       if (!err) req._user_ = decodedData;
       next();
     });
--- server/server.js
+++ server/server.js
@@ -2,9 +2,11 @@ import express from "express";
 import path from "path";
 import mongoose from "mongoose";
 import cookieParser from "cookie-parser";
+import WebSocket from "ws";
 
 import authRouter from "./routers/auth";
 import userRouter from "./routers/user";
+import { authenticateUserForWS } from "./middlewares/authenticateUser";
 
 //to configure the environment variables
 require("dotenv").config();
@@ -27,24 +29,57 @@ server.use("/auth", authRouter);
 server.use("/user", userRouter);
 
 //servering the index.html file
-server.get("/*", (request, response) => {
+server.get("/", (request, response) => {
   response.sendFile(path.resolve("./build/index.html"));
 });
 
 // db connection
-mongoose.connect(
-  process.env.DB_URL,
-  {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-    useFindAndModify: false,
-    useCreateIndex: true,
-  },
-  (err) => {
-    if (err) console.log(err);
-  }
-);
+// mongoose.connect(
+//   process.env.DB_URL,
+//   {
+//     useNewUrlParser: true,
+//     useUnifiedTopology: true,
+//     useFindAndModify: false,
+//     useCreateIndex: true,
+//   },
+//   (err) => {
+//     if (err) console.log(err);
+//   }
+// );
 
-const wsServer = server.listen(PORT, () =>
+const serverWs = server.listen(PORT, () =>
   console.log(`waiting on port ${PORT}`)
 );
+
+// handling web socket connection
+const wss = new WebSocket.Server({ noServer: true });
+
+wss.on("connection", (ws, _, user) => {
+  console.log("new connection");
+  ws.on("message", (packet) => {
+    wss.clients.forEach((client) => {
+      if (client.readyState === WebSocket.OPEN) client.send(packet);
+    });
+  });
+
+  ws.on("close", () => console.log("disconnected"));
+});
+
+serverWs.on("upgrade", function upgrade(request, socket, head) {
+  if (request.url != "/chat-room") {
+    socket.write("HTTP/1.1 400 Bad Request\r\n\r\n");
+    socket.destroy();
+    return;
+  }
+
+  authenticateUserForWS(request, (err, user) => {
+    if (err || user == null) {
+      socket.write("HTTP/1.1 401 Unauthorized\r\n\r\n");
+      socket.destroy();
+    } else {
+      wss.handleUpgrade(request, socket, head, function done(ws) {
+        wss.emit("connection", ws, request, user);
+      });
+    }
+  });
+});
